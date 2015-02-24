cba7342a5abcd8be243a81e9e4d6699afff6c1ad
Ronald van Eede <ronaldvaneede@gmail.com>
2015-02-24 22:07:28 +0100

    Added Oracle JDK 7

--- roles/vagrant-dev.rb
+++ roles/vagrant-dev.rb
@@ -1,10 +1,25 @@
 name "vagrant-dev"
 
+override_attributes( 
+    "java" => { 
+        "install_flavor" => "oracle", 
+        "jdk_version" => "7", 
+        "oracle" => { 
+            "accept_oracle_download_terms" => true 
+        } 
+    },
+    "gradle" => {
+        "version" => "2.3"
+    }
+)
+
 run_list(
     "recipe[apt]",
     "recipe[openssl]",
     "recipe[gradle]",
     "recipe[git]",
     "recipe[maven]",
-    "recipe[docker]"
+    "recipe[docker]",
+    "recipe[java]",
+    "recipe[zshell::default]"
 )
